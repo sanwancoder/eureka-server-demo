e6ce61606cc3fe1f373f59667b0d940e0508ab1b
Woo <10041958+sanwancoder@users.noreply.github.com>
2019-05-25 20:28:58 +0800

    Update EurekaApplication.java

--- src/main/java/com/wyfdc/go/EurekaApplication.java
+++ src/main/java/com/wyfdc/go/EurekaApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * @Author: wuqing
- */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
